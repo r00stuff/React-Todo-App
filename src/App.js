cbdc8b3d55a3371bf0ee24e5f8807a9ba6a99561
r00stuff <raulochoa87@gmail.com>
2021-07-02 15:21:59 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Header from "./Header";
+import TodoList from "./components/TodoList";
 
 function App() {
+
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+
+    var randomNum = Math.floor((Math.random() * 100) +1);
+
+    const fetchQuote = async () =>
+      await fetch(
+        `https://type.fit/api/quotes`
+      )
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data[randomNum].text);
+        console.log(data);
+      });
+
+      fetchQuote();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-app">
+        <Header />
+        <h2 className="quote">{quote}</h2>
+                
+          <TodoList />
+
+      </div>
     </div>
   );
 }
